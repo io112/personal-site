219aed92fd2ec4e5093e6daedf2682274b6e235b
Dmitry Paramonov <dparamonov@ozon.ru>
2022-05-11 17:21:07 +0400

    tests fix

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -12,7 +12,6 @@ import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Index from '../pages/Index';
 import NotFound from '../pages/NotFound';
-import Projects from '../pages/Projects';
 import Resume from '../pages/Resume';
 import Stats from '../pages/Stats';
 
@@ -26,11 +25,6 @@ const pages = [
     route: '/about',
     heading: 'About Me',
     component: About,
-  },
-  {
-    route: '/projects',
-    heading: 'Projects',
-    component: Projects,
   },
   {
     route: '/stats',
